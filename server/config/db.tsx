b5131e6e935570153fbe90b18f5194f926dec081
teodorasimona <ghicateodora12@yahoo.com>
2024-06-08 16:09:07 +0300

    prisma

--- server/config/db.tsx
+++ server/config/db.tsx
@@ -1,26 +1,9 @@
-// const mysql = require("mysql");
+import { PrismaClient } from "@prisma/client";
 
-// const db = mysql.createConnection({
-//   host: "localhost",
-//   user: "root",
-//   password: "root",
-//   database: "trailDB",
-// });
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
 
-// db.connect((err) => {
-//   if (err) {
-//     console.error("Error connecting to the database:", err);
-//     return;
-//   }
-//   console.log("Connected to the database");
-// });
+export const prisma = globalForPrisma.prisma ?? new PrismaClient();
 
-// export default async function excuteQuery({ query, values }) {
-//   try {
-//     const results = await db.query(query, values);
-//     await db.end();
-//     return results;
-//   } catch (error) {
-//     return { error };
-//   }
-// }
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
